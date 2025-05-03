51b45777e3207d3b110777b450cb23d5de3f4acd
KUNJ PATEL <kunjarpatel@gmail.com>
2025-05-02 21:44:00 -0500

    Update encrypt_and_push.py

--- encrypt_and_push.py
+++ encrypt_and_push.py
@@ -6,7 +6,7 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
 # Load and decode service account JSON
-creds_b64 = os.environ['GOOGLE_SERVICE_ACCOUNT_JSON']
+creds_b64 = os.getenv['GOOGLE_SERVICE_ACCOUNT_JSON']
 creds_json = base64.b64decode(creds_b64).decode()
 with open("temp_creds.json", "w") as f:
     f.write(creds_json)
@@ -17,7 +17,7 @@ creds = service_account.Credentials.from_service_account_file("temp_creds.json",
 service = build('sheets', 'v4', credentials=creds)
 
 # Read Sheet
-sheet_id = os.environ['GOOGLE_SHEET_ID']
+sheet_id = os.getenv['GOOGLE_SHEET_ID']
 range_name = "01-01-2025 to 05-02-2025"
 result = service.spreadsheets().values().get(spreadsheetId=sheet_id, range=range_name).execute()
 values = result.get("values", [])
@@ -30,13 +30,13 @@ with open("data.json", "w") as f:
     json.dump(data, f, indent=2)
 
 # Encrypt with GPG
-gpg_input = os.environ['GPG_PRIVATE_KEY']
+gpg_input = os.getenv['GPG_PRIVATE_KEY']
 with open("private.key", "w") as f:
     f.write(gpg_input)
 
 subprocess.run(["gpg", "--batch", "--import", "private.key"])
 subprocess.run([
-    "gpg", "--batch", "--yes", "--passphrase", os.environ["GPG_PASSPHRASE"],
+    "gpg", "--batch", "--yes", "--passphrase", os.getenv["GPG_PASSPHRASE"],
     "-o", "encrypted_data.json.gpg", "-c", "data.json"
 ])
 
